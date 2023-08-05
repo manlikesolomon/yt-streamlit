745c143874608fc4f10b993f7ae97342b73285af
Solomon Balogun <78293216+manlikesolomon@users.noreply.github.com>
2023-08-05 20:41:40 +0100

    Update youtube-st.py
    changed the read locations

--- youtube-st.py
+++ youtube-st.py
@@ -18,7 +18,7 @@ def audince_sample(country):
 # load our data
 @st.cache
 def load_data():
-    df_agg = pd.read_csv('data/Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+    df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                           'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                           'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
@@ -28,9 +28,9 @@ def load_data():
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
     df_agg.sort_values('Video publish time', ascending = False, inplace = True)
-    df_agg_sub = pd.read_csv('data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-    df_comm = pd.read_csv('data/All_Comments_Final.csv')
-    df_time = pd.read_csv('data/Video_Performance_Over_Time.csv')
+    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comm = pd.read_csv('All_Comments_Final.csv')
+    df_time = pd.read_csv('Video_Performance_Over_Time.csv')
     df_time['Date'] = df_time['Date'].str.replace('Sept', 'Sep')
     df_time['Date'] = pd.to_datetime(df_time['Date'], format="%d %b %Y")
     return df_agg, df_agg_sub, df_comm, df_time
